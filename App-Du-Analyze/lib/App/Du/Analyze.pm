1254cd8297dde483110ff9d2fa9cae2d97d9efe6
Shlomi Fish <shlomif@shlomifish.org>
2014-04-29 21:10:14 +0300

    Got the tests to pass.

--- App-Du-Analyze/lib/App/Du/Analyze.pm
+++ App-Du-Analyze/lib/App/Du/Analyze.pm
@@ -3,5 +3,12 @@ package App::Du::Analyze;
 use strict;
 use warnings;
 
+our $VERSION = '0.0.1';
+
 1;
+
+=head1 NAME
+
+App::Du::Analyze - analyze the output of du and find the most space-consuming
+directories.
 
